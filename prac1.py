5b6bff64480aa14e1e1636d88aebacfd3b421fb6
Raukonim <fajulanet@gmail.com>
2015-02-17 17:36:42 +0100

    P1 comentada

--- prac1.py
+++ prac1.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Wed Feb 11 10:10:54 2015
 
-This is a temporary script file.
+@author: raukonim
 """
 
 from pylab import *
@@ -11,9 +11,17 @@ from pylab import *
 close('all')
 interactive(True)
 
+# Lectura de la imatge original en double per tal de poder fer les
+# operacions necessaries sense preocupar-nos de comes flotants i altres
 im=double(imread("tormentadeespadas.jpg"))
+# Prenem la mida a la imatge per si necessitem emprarles per generar-ne altres
 dimen=im.shape
 
+"""
+Creem tres imatges a color amb cadascuna de les capes de la imatge original.
+Per poder-ho fer necessitem la mida original, dimen, i les hem d'omplir
+de zeros ja que sols emprarem 1 capa de color mentre les altrs son zero. 
+"""
 imrc=zeros([dimen[0],dimen[1], dimen[2]])#,"uint8")
 imrc[:,:,0]=im[:,:,0]
 imgc=zeros([dimen[0],dimen[1], dimen[2]])#,"uint8")
@@ -21,18 +29,23 @@ imgc[:,:,1]=im[:,:,1]
 imbc=zeros([dimen[0],dimen[1], dimen[2]])#,"uint8")
 imbc[:,:,2]=im[:,:,2]
 
+# Creem tres imatges BN una amb cada capa
 imr=im[:,:,0]
 img=im[:,:,1]
 imb=im[:,:,2]
 
+# Generem una imatge BN mitjana
 ng=0.333*(imr+img+imb)
 ng=uint8(ng)
 
+# Generem la imatge luminància amb cada color correctament pesat
 L=((.299)*imr)+((.587)*img)+((.114)*imb)
 L=uint8(L)
 
+# Generem la imatge diferència
 D=L-ng
 
+# Convertim totes les imatges a enters de 8 bits
 im=uint8(im)
 imr=uint8(imr)
 img=uint8(img)
@@ -41,6 +54,7 @@ imrc=uint8(imrc)
 imgc=uint8(imgc)
 imbc=uint8(imbc)
 
+# Mostres totes les figures per pantalla
 """
 figure(0)
 imshow(im)
@@ -72,6 +86,7 @@ imshow(L, cmap="gray")
 figure(9)
 imshow(D, cmap="jet")"""
 
+# Guardem les imatges
 imsave("fig1_imrc.png", imrc)
 imsave("fig2_imgc.png", imgc)
 imsave("fig3_imbc.png", imbc)
