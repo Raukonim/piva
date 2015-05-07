114a2b76c5ee12cd705b01f977cf51ebf2b21df6
Albert Fajula <fajulanet@gmail.com>
2015-05-07 13:04:14 +0200

    Alfanumeric encoding

--- qr.py
+++ qr.py
@@ -10,7 +10,12 @@ from pylab import *
 
 s="HELLO WORLD"
 
-
+alfanumeric_table={
+    '0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,
+    'A':10,'B':11,'C':12,'D':13,'E':14,'F':15,'G':16,'H':17,'I':18,
+    'J':19,'K':20,'L':21,'M':22,'N':23,'O':24,'P':25,'Q':26,'R':27,
+    'S':28,'T':29,'U':30,'V':31,'W':32,'X':33,'Y':34,'Z':35,' ':36,
+    '$':37,'%':38,'*':39,'+':40,'-':41,'.':42,'/':43,':':44}
 
 #s=zfill(9)
 
@@ -23,13 +28,15 @@ class qrcode:
         self.length = len(self.data)
         self.bin_len = bin(self.length)[2:]
         self.qr_matrix = [[0],[0]]
+        self.pad_len=0
+        self.car_count=9
         
         
+    #def caracter_count_indicator(self, version, encoding):
+    #    self.car_count=9
 
-    def car_count_calc(self, self.version)
-        
-        
-        self.pad_len=self.bin_len.zfill(car_count)
+    def car_count_calc(self):
+        self.pad_len=self.bin_len.zfill(self.car_count)
     
     def define_encoding(self, enc):
         self.encoding=enc
@@ -40,8 +47,25 @@ class qrcode:
     def matrix_init(self):
         self.qr_matrix=zeros([21+(4*(self.version-1))], [21+(4*(self.version-1))])
     
-    #def caracter_count_indicator(self, version, encoding):
-    #    car_count=9
+    def alphanumeric_encoding(self):
+        z=''
+        for i in range(0,self.length,2):
+            x=''
+            x=self.data[i:i+2]
+            print x
+            if len(x)==1:
+                y=bin(alfanumeric_table[x])[2:]
+                y=y.zfill(6)
+            else:
+                y=bin((45*alfanumeric_table[x[0]])+alfanumeric_table[x[1]])[2:]
+                y=y.zfill(11)
+            print y
+            z+=y
+        print z
     
     
 hello=qrcode(s)
+
+hello.car_count_calc()
+
+hello.alphanumeric_encoding()
